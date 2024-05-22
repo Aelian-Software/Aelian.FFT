709eb45f1e99677a49a380a8ed1c30b40d97f09d
Bas Tossings <12195470+BasTossings@users.noreply.github.com>
2024-05-22 12:25:49 +0200

    Decreased assertion precision and added a test for complex-valued transforms

--- src/Tests/ValidateOutputTests.cs
+++ src/Tests/ValidateOutputTests.cs
@@ -1,10 +1,13 @@
+using System.Numerics;
+using System.Runtime.InteropServices;
+
 using Xunit.Abstractions;
 
 namespace Tests
 	{
 	public class ValidateOutputTests : IClassFixture<FftTestFixture>
 		{
-		private const int AssertPrecision = 8;
+		private const int AssertPrecision = 7;
 		private readonly ITestOutputHelper _Output;
 		private readonly FftTestFixture _Fixture;
 
@@ -15,7 +18,38 @@ namespace Tests
 			}
 
 		[Fact]
-		public void TestAgainstLomont ()
+		public void TestComplexAgainstLomont ()
+			{
+			var ComplexValues = _Fixture.ComplexInputData;
+
+			// Aelian forward FFT
+
+			var AelianBuffer = (Complex[]) ComplexValues.Clone ();
+			Aelian.FFT.FastFourierTransform.FFT ( AelianBuffer, true );
+
+			// Lomont forward FFT
+
+			var Lomont = new Lomont.LomontFFT () { A = 1, B = -1 };
+			var LomontBuffer = (Complex[]) ComplexValues.Clone ();
+			var InterleavedLomontBuffer = MemoryMarshal.Cast<Complex, double> ( LomontBuffer );
+			Lomont.FFT ( InterleavedLomontBuffer, true );
+
+			// Verify that forward transform yielded equal (or close enough) values
+
+			TestHelpers.AssertEqual ( LomontBuffer, AelianBuffer, AssertPrecision );
+
+			// Inverse FFT
+
+			Aelian.FFT.FastFourierTransform.FFT ( AelianBuffer, false );
+			Lomont.FFT ( InterleavedLomontBuffer, false );
+
+			// Verify that inverse transform yielded equal (or close enough) values
+
+			TestHelpers.AssertEqual ( LomontBuffer, AelianBuffer, AssertPrecision );
+			}
+
+		[Fact]
+		public void TestRealAgainstLomont ()
 			{
 			var RealValues = _Fixture.RealInputData;
 
