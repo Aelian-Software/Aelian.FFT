1e5692cd2c499422c9d728a46e1d1349f0ddfe34
Bas Tossings <12195470+Sebtous@users.noreply.github.com>
2026-06-26 09:59:48 +0200

    Updated copyright messages

--- src/Aelian.FFT/MathUtils.cs
+++ src/Aelian.FFT/MathUtils.cs
@@ -7,7 +7,7 @@
 //
 // MIT License
 //   
-// Copyright (c) 2024 Bas Tossings / Aelian
+// Copyright (c) 2026 Bas Tossings / Aelian
 //   
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
